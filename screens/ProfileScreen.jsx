0edc377ea372d56c9e3a6259bb7798d372563ef9
Benjamin Hall <hallbkb@gmail.com>
2025-04-26 17:07:54 -0600

    Changed tab size

--- screens/ProfileScreen.jsx
+++ screens/ProfileScreen.jsx
@@ -4,39 +4,36 @@ import { collection, onSnapshot, query } from 'firebase/firestore';
 import { db } from '../firebaseConfig';
 
 export default function ProfileScreen({ route }) {
+    const { usrId } = route.params;
+    const [reviews, setReviews] = useState([]);
+    console.log(usrId);
 
-  const { usrId } = route.params
-  const [reviews, setReviews] = useState([]);
-  console.log(usrId);
+    // Fetch user's most recent reviews
+    useEffect(() => {
+        const q = query(collection(db, 'Reviews_By_User_ID', usrId, 'Reviews'));
+        const unsubscribe = onSnapshot(q, (querySnapshot) => {
+            const ReviewsData = querySnapshot.docs.map(doc => doc.data());
+            setReviews(ReviewsData);
+        });
 
+        return () => unsubscribe();
+    }, [usrId]);
 
-  // Fetch users most recent reviews
-  useEffect(() => {
-  //const unsubscribe = db.collection('establishments').doc(place.id)
-  const q = query(collection(db, 'Reviews_By_User_ID', usrId, 'Reviews'));
-  const unsubscribe = onSnapshot(q, (querySnapshot) => {
-    const ReviewsData = querySnapshot.docs.map(doc => doc.data());
-    setReviews(ReviewsData);
-  });
-
-  return () => unsubscribe();
-  }, [usrId]);
-    
-
-  return (
-    <View style={styles.container}>
-      <Text styles={styles.subtitle}>Username:</Text>
-      <Text styles={styles.subtitle}>Reviews:</Text>
-        {reviews.map((item, index) => (
-          <Text key={index}>{item.establishmentName}: {item.rating} - {item.comment}</Text>
-        ))}
-    </View>
-  );
+    return (
+        <View style={styles.container}>
+            <Text style={styles.subtitle}>Username:</Text>
+            <Text style={styles.subtitle}>Reviews:</Text>
+            {reviews.map((item, index) => (
+                <Text key={index}>
+                    {item.establishmentName}: {item.rating} - {item.comment}
+                </Text>
+            ))}
+        </View>
+    );
 }
 
-
 const styles = StyleSheet.create({
-  container: { flex: 1 },
-  subtitle: { fontSize: 18, marginTop: 10 },
-  input: { borderWidth: 1, padding: 5, marginVertical: 5 },
+    container: { flex: 1 },
+    subtitle: { fontSize: 18, marginTop: 10 },
+    input: { borderWidth: 1, padding: 5, marginVertical: 5 },
 });
